6a1f6388ea77054d22470560eb9376f3e3ac18bb
Mary <marieshix98@gmail.com>
2022-10-03 14:29:07 +0300

    update location array

--- backend/location.js
+++ backend/location.js
@@ -1,7 +1,7 @@
 const location = [
-  { id: 1, site: "supermarket", Coffee: "ksh 3000" },
-  { id: 2, site: "hpermarket", Coffee: "ksh 3500" },
-  { id: 3, site: "shop", Coffee: "ksh 2800" },
+  { id: 1, site: "supermarket" },
+  { id: 2, site: "hpermarket" },
+  { id: 3, site: "shop" },
 ];
 
 module.exports = location;
