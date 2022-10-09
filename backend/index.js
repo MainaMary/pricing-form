761e1cc6bd60d4d8e9cee300dae812c90cdfe203
Mary <marieshix98@gmail.com>
2022-10-09 19:24:10 +0300

    Add products model

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Price = require("./models/priceForm");
+const Product = require("./models/product");
+const joi = require("joi");
 require("dotenv").config();
 const app = express();
 const port = 7000 || process.env.PORT;
 const connection_string = process.env.CONNECTION_STRING;
 const location = require("./location");
+const { number } = require("joi");
 
 console.log(Price);
 //configure middleware
 app.use(express.json());
 app.use(cors());
-
+//define joi schema
+const schema = joi.object({
+  name: joi.string().required(),
+  productId: joi.string(),
+  taxation: joi.number(),
+  discount: joi.number(),
+  subsidy: joi.number(),
+  total: joi.number(),
+});
 app.get("/", (req, res) => {
   res.send(location);
 });
