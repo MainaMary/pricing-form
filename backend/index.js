e821c2fe6f6bf1d4d0a261b1ff8f4b274b64156d
Mary <marieshix98@gmail.com>
2022-10-10 18:23:08 +0300

    Handle Create , update and delete requests

--- backend/index.js
+++ backend/index.js
@@ -3,30 +3,25 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const Price = require("./models/priceForm");
 const Product = require("./models/product");
-const joi = require("joi");
+const routes = require("./routes/prices");
 require("dotenv").config();
 const app = express();
-const port = 7000 || process.env.PORT;
+const port = 5000 || process.env.PORT;
 const connection_string = process.env.CONNECTION_STRING;
 const location = require("./location");
-const { number } = require("joi");
 
 console.log(Price);
 //configure middleware
 app.use(express.json());
 app.use(cors());
-//define joi schema
-const schema = joi.object({
-  name: joi.string().required(),
-  productId: joi.string(),
-  taxation: joi.number(),
-  discount: joi.number(),
-  subsidy: joi.number(),
-  total: joi.number(),
-});
+
+// all routes start with  /productsVarieties
+app.use("/", routes);
+
 app.get("/", (req, res) => {
   res.send(location);
 });
+
 app.listen(port, console.log(`Server is running on port ${port}`));
 mongoose
   .connect(connection_string)
--- backend/routes/prices.js
+++ backend/routes/prices.js
@@ -1,18 +1,40 @@
-import { Price } from "../models/priceForm";
+const Price = require("../models/priceForm");
 const express = require("express");
-const Joi = express("joi");
-const router = express.json();
+const Joi = require("joi");
+const {
+  findByIdAndDelete,
+  findById,
+  findByIdAndUpdate,
+} = require("../models/priceForm");
+const router = express.Router();
 
-const schema = Joi.object({
-  name: Joi.string().required(),
-  productId: Joi.string(),
-  taxation: Joi.number(),
-  discount: Joi.number(),
-  subsidy: Joi.number(),
-  total: Joi.number(),
-});
-router.post("/", async (req, res) => {
-  const { name, productId, index, taxation, discount, subsidy } = req.body;
+const handleValidation = (req) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    productId: Joi.string(),
+    taxation: Joi.number(),
+    discount: Joi.number(),
+    subsidy: Joi.number(),
+    total: Joi.number(),
+    index: Joi.number(),
+  });
+  const { error } = schema.validate(req);
+  if (error) return res.status(400).send("Validation error");
+};
+router.post("/productsVarieties", async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    productId: Joi.string(),
+    taxation: Joi.number(),
+    discount: Joi.number(),
+    subsidy: Joi.number(),
+    total: Joi.number(),
+    index: Joi.number(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send("Validation error");
+  const { name, productId, index, taxation, discount, subsidy, total, date } =
+    req.body;
   const todo = new Price({
     name,
     productId,
@@ -21,6 +43,7 @@ router.post("/", async (req, res) => {
     discount,
     subsidy,
     total,
+    date,
   });
   try {
     const response = await todo.save();
@@ -29,6 +52,44 @@ router.post("/", async (req, res) => {
     //sending error to the client
     res.status(500).send(error.message);
     console.log(error.message);
+  }
+});
+
+router.get("/productsVarieties:id", async (req, res) => {
+  const response = await Price.find().sort({ date: -1 });
+  res.send(response);
+});
+router.delete("/productVarieties", async (req, res) => {
+  try {
+    const deletedVariety = await findByIdAndDelete(req.params.id);
+    res.send(deletedVariety);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.put("/productVarieties:id", async (req, res) => {
+  handleValidation(req.body);
+  try {
+    const variety = findById(req.body.params);
+    if (!variety) return res.status(404).send("Variety not found");
+    const updateVariety = await findByIdAndUpdate(
+      req.body.params,
+      {
+        name,
+        productId,
+        index,
+        taxation,
+        discount,
+        subsidy,
+        total,
+        date,
+      },
+      { new: true }
+    );
+    res.send(updateVariety);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 module.exports = router;
