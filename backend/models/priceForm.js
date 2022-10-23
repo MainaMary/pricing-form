72b1be5039a4e06749df7d42e3bffcd3f7aafd16
Mary <marieshix98@gmail.com>
2022-10-23 17:47:03 +0300

    Add timestamp on the price schema

--- backend/models/priceForm.js
+++ backend/models/priceForm.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const priceSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  productId: String,
-  index: Number,
-  taxation: Number,
-  discount: Number,
-  subsidy: Number,
-  total: Number,
-  date: { type: Date, default: new Date() },
-});
+const priceSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    productId: String,
+    index: Number,
+    taxation: Number,
+    discount: Number,
+    subsidy: Number,
+    total: Number,
+    // date: { type: Date, default: new Date() },
+  },
+  { timestamps: true }
+);
 const Price = mongoose.model("Price", priceSchema);
 
 module.exports = Price;
